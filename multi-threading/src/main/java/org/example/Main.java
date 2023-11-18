2539e553a13fd3c15361c713b11b4a76d963fc51
rishikeshtadaka <rishikeshtadaka@gmail.com>
2023-11-18 10:09:48 +0530

    Added time logs

--- multi-threading/src/main/java/org/example/Main.java
+++ multi-threading/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    private static Integer tillNumber=1000;
+    private static Integer tillNumber=1000000;
     private static Integer counter=0;
     private static void checkPrime(Integer number){
         for(Integer i=2;i<=Math.sqrt(number);i++){
@@ -12,9 +15,20 @@ public class Main {
         counter++;
     }
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        System.out.println("*********************Started processing***********************");
         for(Integer i=2;i<tillNumber;i++){
             checkPrime(i);
         }
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        long totalMilliSeconds= TimeUnit.NANOSECONDS.toMillis(totalTime);
+        long totalSeconds= TimeUnit.NANOSECONDS.toSeconds(totalTime);
+
         System.out.println("Total prime numbers:"+counter);
+        System.out.println("Total time(non-seconds):"+totalTime);
+        System.out.println("Total time(milli-seconds):"+totalMilliSeconds);
+        System.out.println("Total time(seconds):"+totalSeconds);
+        System.out.println("*********************Completed processing***********************");
     }
 }
